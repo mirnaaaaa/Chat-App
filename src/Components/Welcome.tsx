b88c46bfa4010a697ca2abde3cdd341d1972008b
mirnaaaaa <mirnamisheal1@yahoo.com>
2023-02-17 15:06:23 +0200

    first commit

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { User, UserType } from "../Context/User";
+import { useEffect } from "react";
 
 export const Welcome = () => {
+  const { docId } = useContext(User) as UserType;
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!docId) {
+      navigate("/Login");
+    }
+  }, []);
+
   return (
-    <div className='background'>
-<h1>Send and receive messages without keeping your phone online.</h1>
+    <div className="background">
+      <h1>Send and receive messages without keeping your phone online.</h1>
     </div>
-  )
-}
+  );
+};
